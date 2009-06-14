737a2d1998f7c01ca15c813433b33a5fd7e7c3ba
John <john@theworkinggroup.ca>
2009-06-14 13:55:40 +0100

    fix

--- init.rb
+++ init.rb
@@ -1,6 +1,2 @@
-#gems required
-require 'rdiscount'
-require 'redcloth'
-
 #load library
 require 'render_helpers'
--- lib/render_helpers.rb
+++ lib/render_helpers.rb
@@ -1,12 +1,17 @@
 module RenderHelpers
-  def text_filter(body, filter_id)
-    case filter_id.to_i
-      when 1
-        return body.to_s
-      when 2
-        return RDiscount.new(body).to_html
-      when 3
-        return RedCloth.new(body).to_html
+end
+
+class String
+  def text_filter(type = :plain)
+    case type
+      when :plain
+        return self.to_s
+      when :markdown
+        require 'rdiscount'
+        return RDiscount.new(self).to_html
+      when :textile
+        require 'redcloth'
+        return RedCloth.new(self).to_html
     end
   end
 end
